8cd40943d00f83ca9133b2b5eb8c1cf6f7dbb17f
Luis-f4 <luisbittner1.0@gmail.com>
2024-08-02 11:17:57 +0200

    Zweiter commit Luis

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 //Test 1
+//test
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -6,7 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      
+
     <div className="App">
       <Navbar />
       <div className="content">

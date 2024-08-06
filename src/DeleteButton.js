3fc433b187f50e24b93fa3bcd9837608b55b838c
Luis-f4 <luisbittner1.0@gmail.com>
2024-08-06 09:31:30 +0200

    Delte Funktioniert

--- src/DeleteButton.js
+++ src/DeleteButton.js
@@ -1,20 +1,22 @@
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate, useParams } from 'react-router-dom'; 
+
 import { useState } from 'react';
 
 const DeleteButton = ({ mitarbeiterId, typeOfMA }) => {
     const navigate = useNavigate(); 
     const [isPending, setIsPending] = useState(false);
+    const{ typeOfMitarbeiter } = useParams();
 
     const handleDelete = () => {
         setIsPending(true);
 
         let url = '';
 
-        console.log("typeOfMA: " + typeOfMA + "  mitarbeiterId: " + mitarbeiterId);
+        console.log("Delete Button. typeOfMA: " + typeOfMitarbeiter + "  mitarbeiterId: " + mitarbeiterId);
 
-        if (typeOfMA === "Mitarbeiter") {
+        if (typeOfMitarbeiter === "Mitarbeiter") {
             url = `http://localhost:8080/api/v1/Mitarbeiter/${mitarbeiterId}`;
-        } else if (typeOfMA === "MitarbeiterMarketing") {
+        } else if (typeOfMitarbeiter === "MitarbeiterMarketing") {
             url = `http://localhost:8080/api/v1/MitarbeiterMarketing/${mitarbeiterId}`;
         }
 
@@ -23,7 +25,7 @@ const DeleteButton = ({ mitarbeiterId, typeOfMA }) => {
         }).then(() => {
             console.log(`${typeOfMA} deleted`);
             setIsPending(false);
-            navigate('/'); // Verwende navigate statt history.push
+            navigate('/'); 
         }).catch((error) => {
             console.error(`Error deleting ${typeOfMA}:`, error);
             setIsPending(false);

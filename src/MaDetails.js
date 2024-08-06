97ef5223b9eb9829925cb1f9c128717f6391dfed
Luis-f4 <luisbittner1.0@gmail.com>
2024-08-06 12:33:00 +0200

    Alle CRUD-Funktionen funktionieren - style fehlt noch

--- src/MaDetails.js
+++ src/MaDetails.js
@@ -2,12 +2,12 @@ import { useParams } from 'react-router-dom';
 import UpdateButton from './UpdateButton';
 import Getone from "./Getone";
 import DeleteButton from './DeleteButton';
+import { useState, useEffect } from "react";
 
 const MaDetails = () => {
     const { id, typeOfMitarbeiter } = useParams();
 
     let url;
-   
 
     if (typeOfMitarbeiter === "Mitarbeiter") {
         url = 'http://localhost:8080/api/v1/Mitarbeiter/';
@@ -16,6 +16,16 @@ const MaDetails = () => {
     }
 
     const { data: mitarbeiter, isPending } = Getone(url + id);
+
+    const [name, setName] = useState('');
+    const [abteilung, setAbteilung] = useState('');
+
+    useEffect(() => {
+        if (mitarbeiter) {
+            setName(mitarbeiter.name);
+            setAbteilung(mitarbeiter.abteilung);
+        }
+    }, [mitarbeiter]);
 
     return (
         <div>
@@ -26,29 +36,50 @@ const MaDetails = () => {
                     <div className="daten-ma-div">
                         <div className="name-ma-div">
                             <label className="name-ma-label">Name</label>
-                            <input className="name-ma-input" type="text" value={mitarbeiter.name}  />
+                            <input 
+                                className="name-ma-input" 
+                                type="text" 
+                                value={name} 
+                                onChange={(e) => setName(e.target.value)} 
+                            />
                         </div>
                         <div className="dob-ma-div">
                             <label className="dob-ma-label">Date of Birth</label>
-                            <input className="dob-ma-input" type="text" value={mitarbeiter.dob} readOnly />
+                            <input 
+                                className="dob-ma-input" 
+                                type="text" 
+                                value={mitarbeiter.dob} 
+                                readOnly 
+                            />
                         </div>
                         <div className="abteilung-ma-div">
                             <label className="abteilung-ma-label">Abteilung</label>
-                            <input className="abteilung-ma-input" type="text" value={mitarbeiter.abteilung}  />
+                            <input 
+                                className="abteilung-ma-input" 
+                                type="text" 
+                                value={abteilung}  
+                                onChange={(e) => setAbteilung(e.target.value)}
+                            />
                         </div>
                         <div className="unternehmen-ma-div">
                             <label className="unternehmen-ma-label">Unternehmen</label>
-                            <input className="unternehmen-ma-input" type="text" value={mitarbeiter.unternehmenID} readOnly />
+                            <input 
+                                className="unternehmen-ma-input" 
+                                type="text" 
+                                value={mitarbeiter.unternehmenID} 
+                                readOnly 
+                            />
                         </div>
                     </div>
                     <UpdateButton
                         mitarbeiterId={mitarbeiter.id}
-                        typeOfMA={mitarbeiter.typeOfMA}
-                        abteilung={mitarbeiter.abteilung}
+                        typeOfMA={typeOfMitarbeiter}
+                        abteilung={abteilung}
+                        name={name}
                     />
                     <DeleteButton
                         mitarbeiterId={mitarbeiter.id}
-                        typeOfMA={mitarbeiter.typeOfMA}
+                        typeOfMA={typeOfMitarbeiter}
                     />
                 </div>
             )}
--- src/UpdateButton.js
+++ src/UpdateButton.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom'; 
 
-const UpdateButton = ({ mitarbeiterId, typeOfMA, abteilung }) => {
+const UpdateButton = ({ mitarbeiterId, typeOfMA, abteilung, name }) => {
     const [isPending, setIsPending] = useState(false);
     const{ typeOfMitarbeiter } = useParams();
 
@@ -9,15 +9,16 @@ const UpdateButton = ({ mitarbeiterId, typeOfMA, abteilung }) => {
     const handleUpdate = () => {
         let url = '';
 
-        console.log("Update Button neueabteilung: " + abteilung + "  mitarbeiterId: " + mitarbeiterId + " Type ofMa: " + typeOfMitarbeiter);
+        console.log("Update Button neueabteilung: " + abteilung + "  mitarbeiterId: " + mitarbeiterId + " Type ofMa: " + typeOfMitarbeiter + "  neuer name: " +  name);
         
         
         if(typeOfMitarbeiter === 'Mitarbeiter'){
-            url = `http://localhost:8080/api/v1/Mitarbeiter/${mitarbeiterId}?abteilung=${abteilung}`;
+            url = `http://localhost:8080/api/v1/Mitarbeiter/${mitarbeiterId}?abteilung=${abteilung}&name=${name}`;
         } else if(typeOfMitarbeiter === 'MitarbeiterMarketing'){
-            url = `http://localhost:8080/api/v1/MitarbeiterMarketing/${mitarbeiterId}?abteilung=${abteilung}`;
+            url = `http://localhost:8080/api/v1/MitarbeiterMarketing/${mitarbeiterId}?abteilung=${abteilung}&name=${name}`;
         }
 
+        
         
 
         setIsPending(true);
